abbf84f085b702244ec197b0c12964381ebb4231
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-03-20 13:32:18 +0530

    add name to dex_info.sql

--- dbt_subprojects/dex/models/dex_info.sql
+++ dbt_subprojects/dex/models/dex_info.sql
@@ -210,7 +210,5 @@ FROM (VALUES
     , ('beraswap', 'BeraSwap', 'Direct', 'BeraSwap')
     , ('burrbear', 'BurrBear', 'Direct', 'moneygoesburr')
     , ('reservoir_swap', 'Reservoir Swap', 'Direct', 'reservoir0x')
-    , ('inkyswap', 'InkySwap', 'Direct', 'inkyswap')
-    , ('squidswap', 'SquidSwap', 'Direct', 'squidswap_ink')
-    , ('swanswap', 'SwanSwap', 'Direct', 'swan_swap')
+    , ('tamadotmeme', 'tamadotmeme', 'Direct', 'tamadotmeme')
 ) AS temp_table (project, name, marketplace_type, x_username)
